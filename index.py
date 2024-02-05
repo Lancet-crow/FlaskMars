35cc16c45f3f2bf18a89dec991d9927f62d7f901
NaN <NaN>
2024-02-05 19:43:19 +0400

    Added Promotion Image page

--- index.py
+++ index.py
@@ -64,5 +64,36 @@ def return_mars_image_page():
                 </html>"""
 
 
+@app.route('/promotion_image')
+def return_promotion_image_page():
+    lines = ("Человечество вырастает из детства",
+             "Человечеству мала одна планета.",
+             "Мы сделаем обитаемыми безжизненные пока планеты.",
+             "И начнем с Марса!",
+             "Присоединяйся!")
+    url_style = url_for('static', filename='css/style.css')
+    url_image = url_for('static', filename='img/mars.png')
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_style}" />
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_image}"</img>
+                    <div class="alert alert-secondary">{lines[0]}</div>
+                    <div class="alert alert-success">{lines[1]}</div>
+                    <div class="alert alert-secondary">{lines[2]}</div>
+                    <div class="alert alert-warning">{lines[3]}</div>
+                    <div class="alert alert-danger">{lines[4]}</div>
+                  </body>
+                </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
