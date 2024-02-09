5232123ecee402a369f2b66ce48f5bedd85e5f83
NaN <NaN>
2024-02-09 19:23:38 +0400

    Задача "Готовимся к миссии". Исправил ссылку на html-файл

--- index.py
+++ index.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('base.html', title='Главная страница')
+    return render_template('index.html', title='Главная страница')
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
