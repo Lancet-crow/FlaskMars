dc462920a66599928b823dc8ac40495d9709450f
NaN <NaN>
2024-02-12 18:31:47 +0400

    Задача "Автоматический ответ"

--- index.py
+++ index.py
@@ -23,5 +23,13 @@ def list_prof(list):
     return render_template('list_prof.html', title='Список профессий', list=list, professions=professions)
 
 
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    params = {"title": "Анкета", "surname": "Watny", "name": "Mark", "education": "выше среднего",
+              "profession": "штурман марсохода", "sex": "male", "motivation": "Всегда мечтал застрять на Марсе!",
+              "ready": "True"}
+    return render_template('auto_answer.html', **params)
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
