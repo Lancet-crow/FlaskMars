4d06e3e8439667f8bb6bc74644cda7a87cd35d84
NaN <NaN>
2024-03-04 19:22:59 +0400

    Ещё одна модель

---
+++ data/departments.py
@@ -0,0 +1,17 @@
+import sqlalchemy
+from sqlalchemy import orm
+
+from data.db_session import SqlAlchemyBase
+
+
+class Department(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
+    chief = sqlalchemy.Column(sqlalchemy.Integer)
+    members = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True)
+    user = orm.relationship('User')
